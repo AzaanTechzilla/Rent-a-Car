47de14d7272cfac98529d0d3838be07a9de900ed
AzaanTechzilla <azaan.khalid@techzillaglobal.com>
2025-05-27 17:08:23 +0500

    home page banner slider

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -9,17 +9,41 @@ import {
 } from "@/components/ui/carousel";
 import { profile } from "@/constants/appdata";
 import Autoplay from "embla-carousel-autoplay";
+import {
+  Card,
+  CardContent,
+} from "@/components/ui/card"
+
 
 const CarouselComponent = () => {
   return (
     <>
-      <Carousel plugins={
+      <Carousel   plugins={[
         Autoplay({
-        delay:2000
-      })}>
+          delay: 2000,
+        }),
+      ]}>
+      
         <CarouselContent>
           {profile?.map((items, index)=>(
             <CarouselItem key={index} style={{backgroundImage:`url(${items.img})` }} className='w-full h-full bg-cover bg-center pt-5 bg-no-repeat relative pointer-events-none'>
+<div className="absolute top-0 right-0 bottom-0 left-0 bg-[rgba(0,0,0,0.6)] opacity-60"></div>
+<div className="">
+  <Card className='bg-transparent border-none'>
+  <CardContent className='flex items-center justify-center p-6'>
+    <div className="h-[40vh] md:h-[60vh] flex justify-center items-center flex-col relative">
+      <h3 className="mb-1 lg:mb-5 flex items-center gap-6 text-2xl lg:text-5xl">
+        <span  className="text-third-color font-bold transition-[3s]">{items.title}</span>
+      </h3>
+      <p className="text-white text-sm md:text-4xl text-center w-10/12 md:w-8/12">
+      <i className={"fad fa-" + items.icon}></i>
+      <span>{items.text}</span>
+      </p>
+    </div>
+  </CardContent>
+</Card>
+
+</div>
 
             </CarouselItem>
           ))}
---
+++ src/components/TittleText.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TittleText = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TittleText
